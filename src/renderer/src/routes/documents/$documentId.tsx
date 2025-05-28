62a875be4c1fff53cabe2e0c99313ab3b32869a1
wibus-wee <1596355173@qq.com>
2025-05-28 21:54:30 +0800

    revert(renderer/document): remove document component as a revert commit

--- src/renderer/src/routes/documents/$documentId.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { DocumentDetail } from '@renderer/components/documents'
-
-export const Route = createFileRoute('/documents/$documentId')({
-  component: DocumentDetail
-})
